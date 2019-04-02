344e28adefac7f67acb4082b40b749c252f69956
baktiyart <baktiyar.tentimischov@gmail.com>
2019-04-02 23:22:36 +0200

    Added Course, OverviewOfCourses and SgndStudListComponent

---
+++ src/components/Course.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <td>{{course.name}}}</td>
+        <td>{{course.semester}}</td>
+        <td>{{course.quiz}}</td>
+        <td>
+            <input type="button" value="Bearbetien">
+            <input type="button" value="Löschen">
+        </td>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Course',
+  props: {
+    course: {
+      name: String,
+      semester: String,
+      quiz: int
+    }
+  }
+}
+</script>
---
+++ src/components/SgndStudListComponents.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Eingeschrieben Studenten:</p>
+
+    <ul>
+      <li v-for="st in students" >{{st.name}}</li>
+    </ul>
+    <input type="button" value="Zurück zur Übersicht">
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'SgndStudList',
+  props: {
+    students: {
+      name: String
+    }
+  }
+}
+</script>
